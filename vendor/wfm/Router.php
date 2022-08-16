4a562bee7957f7f536dfbb19d90b54199369b81e
Konstantin <wusnoop@gmail.com>
2022-08-16 18:44:15 +0300

    add Router.php v1.1

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -22,6 +22,45 @@ class Router
     }
     public static function dispatch($url)
     {
-        var_dump($url);
+        if (self::matchRoute($url)){
+            echo 'OK';
+        }else {
+            echo 'No';
+        }
+    }
+    public static function matchRoute($url) : bool
+    {
+        foreach (self::$routes as $pattern => $route){
+            if (preg_match("#{$pattern}#", $url, $matches)){
+                foreach ($matches as $k=>$v){
+                    if (is_string($k)){
+                        $route[$k] = $v;
+                    }
+                }
+                if (empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if (!isset($route['admin_prefix'])){
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+                debug($route);
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                debug($route);
+                return true;
+            }
+        } return false;
+    }
+
+    protected static function upperCamelCase($name) : string
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+
+    }
+    protected static function lowerCamelCase($name) : string
+    {
+        return lcfirst(self::upperCamelCase($name));
+
     }
 }
